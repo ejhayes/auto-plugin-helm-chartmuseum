9b4e27687a3be9fd6cc92dc0d529384a8f3a9a8b
Eric Hayes <eric@deployfx.com>
2023-11-01 12:23:17 -0700

    formatting changes

--- src/helm.ts
+++ src/helm.ts
@@ -1,26 +1,26 @@
-import { readdir, rm, mkdir, cp, readFile, writeFile } from "fs/promises";
-import { execPromise, ILogger } from "@auto-it/core";
-import { join } from "path";
+import { readdir, rm, mkdir, cp, readFile, writeFile } from 'fs/promises'
+import { execPromise, ILogger } from '@auto-it/core'
+import { join } from 'path'
 
 enum TOOLS {
-  HELM = "helm",
-  HELM_DOCS = "helm-docs",
+  HELM = 'helm',
+  HELM_DOCS = 'helm-docs',
 }
 
 interface HelmOptions {
-  versionToken: string;
-  useHelmDocs: boolean;
+  versionToken: string
+  useHelmDocs: boolean
 }
 
 interface PrepOptions {
-  repository?: string;
-  recursive: boolean;
-  replaceVersionToken: boolean;
-  replaceFileWithRepository: boolean;
+  repository?: string
+  recursive: boolean
+  replaceVersionToken: boolean
+  replaceFileWithRepository: boolean
 }
 
 export class Helm {
-  options: HelmOptions;
+  options: HelmOptions
 
   constructor(
     private readonly logger: ILogger,
@@ -32,19 +32,19 @@ export class Helm {
       versionToken:
         options.versionToken !== undefined
           ? options.versionToken
-          : "0.0.0-local",
-    };
+          : '0.0.0-local',
+    }
   }
 
   async validateDependencies() {
-    this.logger.log.info("Checking for helm...");
-    await execPromise(TOOLS.HELM, ["version"]);
+    this.logger.log.info('Checking for helm...')
+    await execPromise(TOOLS.HELM, ['version'])
 
     if (this.options.useHelmDocs) {
-      this.logger.log.info("Checking for helm-docs...");
-      await execPromise(TOOLS.HELM_DOCS, ["--version"]);
+      this.logger.log.info('Checking for helm-docs...')
+      await execPromise(TOOLS.HELM_DOCS, ['--version'])
     } else {
-      this.logger.log.info("Skipping check for helm-docs");
+      this.logger.log.info('Skipping check for helm-docs')
     }
   }
 
@@ -55,16 +55,16 @@ export class Helm {
       })
     )
       .filter((i) => i.isFile() && i.name.search(/\.tgz$/) >= 0)
-      .map((i) => join(path, i.name));
+      .map((i) => join(path, i.name))
 
     for (const chart of chartsToPublish) {
-      this.logger.log.info(`Publishing ${chart}`);
+      this.logger.log.info(`Publishing ${chart}`)
       await execPromise(TOOLS.HELM, [
-        "cm-push",
-        ...(forcePush ? ["-f"] : []),
+        'cm-push',
+        ...(forcePush ? ['-f'] : []),
         chart,
         repository,
-      ]);
+      ])
     }
   }
 
@@ -79,63 +79,63 @@ export class Helm {
       replaceFileWithRepository: true,
       replaceVersionToken: true,
       ..._opts,
-    };
-    const chartDirs = await this.getChartDirs(srcPath, opts.recursive);
+    }
+    const chartDirs = await this.getChartDirs(srcPath, opts.recursive)
 
-    await rm(destPath, { recursive: true, force: true });
-    await mkdir(destPath, { recursive: true });
+    await rm(destPath, { recursive: true, force: true })
+    await mkdir(destPath, { recursive: true })
     await cp(srcPath, destPath, {
       recursive: true,
-    });
+    })
 
     // replace all versions with the current version
     for (const chartDir of chartDirs) {
-      const chartPath = join(destPath, chartDir);
+      const chartPath = join(destPath, chartDir)
       if (opts.replaceVersionToken && this.options.versionToken) {
         for (const file of await this.findMatchingChartFiles(chartPath)) {
           await this.inlineReplace(join(chartPath, file), (content) => {
             return content.replace(
-              new RegExp(this.options.versionToken, "ig"),
+              new RegExp(this.options.versionToken, 'ig'),
               version,
-            );
-          });
+            )
+          })
         }
       }
     }
 
     if (this.options.useHelmDocs) {
-      this.logger.log.info("Updating documentation");
-      await execPromise(TOOLS.HELM_DOCS, ["-u", "publish", "-s", "alphanum"]);
+      this.logger.log.info('Updating documentation')
+      await execPromise(TOOLS.HELM_DOCS, ['-u', 'publish', '-s', 'alphanum'])
     } else {
-      this.logger.log.info("Skipping documentation generation");
+      this.logger.log.info('Skipping documentation generation')
     }
 
     for (const chartDir of chartDirs) {
-      await this.prepChart(join(destPath, chartDir), destPath, version, opts);
+      await this.prepChart(join(destPath, chartDir), destPath, version, opts)
     }
 
     for (const chartDir of chartDirs) {
       await rm(join(destPath, chartDir), {
         recursive: true,
         force: true,
-      });
+      })
     }
   }
 
   async inlineReplace(path: string, replacers: (contents: string) => string) {
-    this.logger.log.debug(`Inline replacement for ${path}`);
+    this.logger.log.debug(`Inline replacement for ${path}`)
 
-    const contents = replacers((await readFile(path)).toString());
+    const contents = replacers((await readFile(path)).toString())
 
-    return await writeFile(path, contents);
+    return await writeFile(path, contents)
   }
 
   async findMatchingChartFiles(chartPath: string) {
-    const MATCHER = /(readme\.md)|(chart\.ya?ml)|(chart\.lock)/i;
+    const MATCHER = /(readme\.md)|(chart\.ya?ml)|(chart\.lock)/i
     return (await readdir(chartPath, { withFileTypes: true }))
       .filter((i) => i.isFile())
       .map((i) => i.name)
-      .filter((i) => i.search(MATCHER) >= 0);
+      .filter((i) => i.search(MATCHER) >= 0)
   }
 
   async prepChart(
@@ -144,28 +144,28 @@ export class Helm {
     version: string,
     opts: PrepOptions,
   ) {
-    this.logger.log.info(`Creating chart: ${srcPath} version ${version}`);
+    this.logger.log.info(`Creating chart: ${srcPath} version ${version}`)
 
     // remove charts dir external dependencies
-    await rm(join(srcPath, "charts", "*.tgz"), {
+    await rm(join(srcPath, 'charts', '*.tgz'), {
       recursive: true,
       force: true,
-    });
+    })
 
     // update dependencies
-    await execPromise(TOOLS.HELM, ["dep", "up", srcPath]);
+    await execPromise(TOOLS.HELM, ['dep', 'up', srcPath])
 
     // update file references with repo aliases
     if (opts.replaceFileWithRepository && opts.repository) {
       for (const file of await this.findMatchingChartFiles(srcPath)) {
         await this.inlineReplace(join(srcPath, file), (contents) => {
-          return contents.replace(/file:\/\/[^\s]+/g, `'${opts.repository}'`);
-        });
+          return contents.replace(/file:\/\/[^\s]+/g, `'${opts.repository}'`)
+        })
       }
     }
 
     // package the chart
-    await execPromise(TOOLS.HELM, ["package", srcPath, "-d", destPath]);
+    await execPromise(TOOLS.HELM, ['package', srcPath, '-d', destPath])
   }
 
   async getChartDirs(path: string, recursive = false) {
@@ -177,9 +177,9 @@ export class Helm {
         })
       )
         .filter((i) => i.isDirectory())
-        .map((i) => i.name);
+        .map((i) => i.name)
     }
 
-    return path;
+    return path
   }
 }
